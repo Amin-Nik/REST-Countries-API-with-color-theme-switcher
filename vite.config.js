7e933e9778854bb9c1b6ee87d62f5c8bca964f77
mohammad-amin-nik-hedayati <amin.nik777@gmail.com>
2024-07-17 00:48:54 +0330

    deployment stuff

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/REST-Countries-API-with-color-theme-switcher/"
 })
